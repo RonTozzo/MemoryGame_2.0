dbdf309b336952e16daa131f924befa4491b157c
Ronald Tozzo <ronaldtozzo@hotmail.com>
2021-06-24 18:36:35 -0300

    add def's and other variables

--- MemoryGame.py
+++ MemoryGame.py
@@ -29,3 +29,52 @@ is_winner = False
 is_letters = False  
 
 # TODO: Adiciona pontuações
+
+def fill_random_colors(n, color_range):
+    lst = []
+    for i in range(n):
+        rand_0_255 = lambda: random.randint(color_range[0], color_range[1]) * 10
+        lst.append((rand_0_255(), rand_0_255(), rand_0_255()))
+    return lst
+
+
+colors = fill_random_colors((DIM_X * DIM_Y) // 2, level_colors_ranges[current_level])
+
+
+class BaseCard:
+    def __init__(self, screen, left_top_coord, x_size, y_size):
+        self.left_top_coord = left_top_coord
+        self.x_size = x_size
+        self.y_size = y_size
+        self.screen = screen
+        self.left_x_coord = self.left_top_coord[0] * self.x_size
+        self.left_y_coord = self.left_top_coord[1] * self.y_size
+        self.card_rect = pygame.Rect(
+            (self.left_x_coord, self.left_y_coord), (self.x_size, self.y_size)
+        )
+
+
+class ColorCard(BaseCard):
+    def __init__(self, screen, color, left_top_coord, x_size, y_size, color_index):
+        super().__init__(screen, left_top_coord, x_size, y_size)
+        self.color = color
+        self.font = pygame.font.Font(os.path.join("fonts", "LiberationMono-Bold.ttf"), y_size)
+        self.color_index = color_index
+        self.hide_color_index_after_click = True
+
+    def show_color_index(self):
+        text = self.font.render(str(self.color_index), True, BLACK)
+        text_rect = text.get_rect(
+            center=(
+                self.left_x_coord + self.x_size // 2,
+                self.left_y_coord + self.y_size // 2,
+            )
+        )
+        self.screen.blit(text, text_rect)
+
+    def draw_card(self):
+        pygame.draw.rect(self.screen, self.color, self.card_rect)
+        pygame.draw.rect(self.screen, BLACK, self.card_rect, 4)
+
+    def hide_color_index(self):
+        self.draw_card()
